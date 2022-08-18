a3905a763fc7c5e207bb843a7765cb7c254bad4f
Robert <robertquillaslopez@gmail.com>
2022-08-18 00:22:17 -0500

    base del proyecto creado

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+const rootNode = document.getElementById('root');
+ReactDOM.render(
+  <StrictMode>
+      <App/>
+  </StrictMode>
+, rootNode);
+
+
+
---
+++ src/pages/Home.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const Home = () => {
+    const dbHome = require("../bd/home.json")
+    console.log(dbHome)
+  return (
+    <div>
+        <section
+            id="Video">
+            
+        </section>
+
+        <section
+            id="Presentacion">
+        
+        </section>
+
+        <section
+            id="Noticias">
+        
+        </section>
+
+        <section
+            id="Estadisticas">
+        
+        </section>
+
+        <section
+            id="Categorias">
+        
+        </section>
+
+        <section
+            id="Consejo">
+        
+        </section>
+
+        <section
+            id="Alianza">
+        
+        </section>
+
+        <section
+            id="Publicaciones">
+        
+        </section>
+    </div>
+  )
+}
+
+export default Home
