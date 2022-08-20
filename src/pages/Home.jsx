1a8c7b95319e24ddf11e9e3ba712d38403d67e94
Robert <robertquillaslopez@gmail.com>
2022-08-20 10:34:45 -0500

    taildwind arreglado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
             id="Presentacion">
             <h1> Academia Nacional de Ciencias </h1>
             <p
-                className='presentacion'>
+                className='presentacion text-black w-[500px]'>
                 La Academia Nacional de Ciencias fue fundada el 6 de agosto de 1938 por un grupo de 35 distinguidos científicos peruanos, originalmente con el nombre “Academia Nacional de Ciencias Exactas, Físicas y Naturales de Lima”, para promocionar la investigación científica y la difusión del conocimiento científico.
             </p>
         
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-    theme: {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend:{
       colors: {
         'azulANC': '#0668B8'
       },
-    }
+    },
+  },
+  plugins: [],
 }
